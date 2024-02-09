fe1587d896583c67609d6482a1976753c0cf54dd
Daniil Bakushkin <dev.daniil.bakushkin@gmail.com>
2024-02-09 20:05:50 +0300

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dev.daniil.bakushkin/Desktop/HuffmanArchiver
+# Build directory: /Users/dev.daniil.bakushkin/Desktop/HuffmanArchiver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("_deps/catch2-build")
+subdirs("test")
---
+++ build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
@@ -0,0 +1,3 @@
+_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
+  /Users/dev.daniil.bakushkin/Desktop/HuffmanArchiver/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
+  /Users/dev.daniil.bakushkin/Desktop/HuffmanArchiver/build/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
